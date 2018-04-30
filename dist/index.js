59751b07ed326dfd4edec47a57470eb995e93aca
Sean Soowan Lee <sean@salesbooster.io>
2018-04-30 17:46:22 +0900

    Add dist files

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+// require("./bootstrap-utilities.min.css");
+// import * as React from "react";
+// import * as ReactDOM from "react-dom";
+export { default as Button } from "./button";
+export { Row, Col } from "./grid";
+export { default as Input } from "./input";
+export { App, default as Layout } from "./layout";
