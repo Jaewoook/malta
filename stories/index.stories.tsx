6affa2ff136b3d8c4eb68111d4b332e326308487
Sean Soowan Lee <sean@salesbooster.io>
2018-05-08 10:21:59 +0900

    Remove welcome components in storybook

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,22 +1,6 @@
 import * as React from "react";
-
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-
-const { Button, Welcome } = require("@storybook/react/demo");
 import { App } from "../src/App";
-
-storiesOf("Welcome", module).add("to Storybook", () => <Welcome/>);
-
-storiesOf("Button", module)
-  .add("with text", () => <Button onClick={action("clicked")}>Hello Button</Button>)
-  .add("with some emoji", () => (
-    <Button onClick={action("clicked")}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
 
 storiesOf("App", module)
   .add("to App", () => <App/>);
