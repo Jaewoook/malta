8c1b1b25bde66ed713baf9e2e74e8e06a455335f
Sean Soowan Lee <sean@salesbooster.io>
2018-05-09 17:04:52 +0900

    Remove legacy codes

--- dist/Layout/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var App_1 = require("./App");
-exports.App = App_1.App;
--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,18 @@
 "use strict";
-// require("./bootstrap-utilities.min.css");
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 Object.defineProperty(exports, "__esModule", { value: true });
-// import * as React from "react";
-// import * as ReactDOM from "react-dom";
-var Button_1 = require("./Button");
-exports.Button = Button_1.Button;
-var Grid_1 = require("./Grid");
-exports.Row = Grid_1.Row;
-exports.Col = Grid_1.Col;
 var App_1 = require("./App");
 exports.App = App_1.App;
+var Footer_1 = require("./Footer");
+exports.Footer = Footer_1.Footer;
+var Input_1 = require("./Input");
+exports.Input = Input_1.Input;
+var Navbar_1 = require("./Navbar");
+exports.Navbar = Navbar_1.Navbar;
+var Select_1 = require("./Select");
+exports.Select = Select_1.Select;
+var Text_1 = require("./Text");
+exports.Text = Text_1.Text;
+__export(require("./theme"));
