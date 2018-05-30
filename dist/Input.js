df36a3da1fae64b49ff92d70a96e6c7355773a9a
Sean Soowan Lee <sean@salesbooster.io>
2018-05-30 11:16:25 +0900

    Add margin props, Fix default box-sizing style of Input

--- dist/Input.js
+++ dist/Input.js
@@ -13,39 +13,59 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0)
+            t[p[i]] = s[p[i]];
+    return t;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = require("react");
 var styled_components_1 = require("styled-components");
+var space = require("styled-system").space;
 var theme_1 = require("./theme");
-var Wrapper = styled_components_1.default.div(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  width: 100%;\n"], ["\n  width: 100%;\n"])));
+var Wrapper = styled_components_1.default.div(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  width: 100%;\n  ", "\n"], ["\n  width: 100%;\n  ", "\n"])), space);
 var Label = styled_components_1.default.label(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n  color: ", ";\n  display: inline-block;\n  font-size: ", "px;\n  font-weight: ", ";\n  margin-bottom: ", "px;\n"], ["\n  color: ", ";\n  display: inline-block;\n  font-size: ", "px;\n  font-weight: ", ";\n  margin-bottom: ", "px;\n"])), theme_1.colors.black.dark, theme_1.fontSizes.h4, theme_1.fontWeights.demiLight, theme_1.space[2]);
 var ErrorLabel = Label.extend(templateObject_3 || (templateObject_3 = __makeTemplateObject(["\n  color: ", ";\n  margin-top: ", ";\n"], ["\n  color: ", ";\n  margin-top: ", ";\n"])), theme_1.colors.red, theme_1.space[1]);
-var StyledInput = styled_components_1.default.input(templateObject_4 || (templateObject_4 = __makeTemplateObject(["\n  background-color: ", ";\n  border: solid 1px ", ";\n  border-radius: 2px;\n  box-shadow: 0 2px 6px 2px ", ";\n  color: ", ";\n  font-size: ", "px;\n  font-weight: ", ";\n  height: 60px;\n  padding: 0 ", "px;\n  width: 100%;\n\n  &:disabled {\n    background-color: ", ";\n  }\n\n  &:focus {\n    border: solid 1px ", "\n    outline: none;\n  }\n"], ["\n  background-color: ", ";\n  border: solid 1px ", ";\n  border-radius: 2px;\n  box-shadow: 0 2px 6px 2px ", ";\n  color: ", ";\n  font-size: ", "px;\n  font-weight: ", ";\n  height: 60px;\n  padding: 0 ", "px;\n  width: 100%;\n\n  &:disabled {\n    background-color: ", ";\n  }\n\n  &:focus {\n    border: solid 1px ", "\n    outline: none;\n  }\n"])), theme_1.colors.white, function (props) { return props.error ? "#f51a1a" : theme_1.colors.black._20; }, theme_1.colors.black._3, theme_1.colors.black.dark, theme_1.fontSizes.h3, theme_1.fontWeights.demiLight, theme_1.space[2], theme_1.colors.black._5, function (props) { return props.error ? theme_1.colors.red : theme_1.colors.blue.royal; });
+var StyledInput = styled_components_1.default.input(templateObject_4 || (templateObject_4 = __makeTemplateObject(["\n  background-color: ", ";\n  border: solid 1px ", ";\n  border-radius: 2px;\n  box-shadow: 0 2px 6px 2px ", ";\n  box-sizing: border-box;\n  color: ", ";\n  font-size: ", "px;\n  font-weight: ", ";\n  height: 60px;\n  padding: 0 ", "px;\n  width: 100%;\n\n  &:disabled {\n    background-color: ", ";\n  }\n\n  &:focus {\n    border: solid 1px ", "\n    outline: none;\n  }\n"], ["\n  background-color: ", ";\n  border: solid 1px ", ";\n  border-radius: 2px;\n  box-shadow: 0 2px 6px 2px ", ";\n  box-sizing: border-box;\n  color: ", ";\n  font-size: ", "px;\n  font-weight: ", ";\n  height: 60px;\n  padding: 0 ", "px;\n  width: 100%;\n\n  &:disabled {\n    background-color: ", ";\n  }\n\n  &:focus {\n    border: solid 1px ", "\n    outline: none;\n  }\n"])), theme_1.colors.white, function (props) { return props.error ? "#f51a1a" : theme_1.colors.black._20; }, theme_1.colors.black._3, theme_1.colors.black.dark, theme_1.fontSizes.h3, theme_1.fontWeights.demiLight, theme_1.space[2], theme_1.colors.black._5, function (props) { return props.error ? theme_1.colors.red : theme_1.colors.blue.royal; });
 var Input = /** @class */ (function (_super) {
     __extends(Input, _super);
     function Input() {
         return _super !== null && _super.apply(this, arguments) || this;
     }
-    Input.prototype.renderLabel = function () {
-        var label = this.props.label;
+    Input.prototype.renderLabel = function (label) {
         if (label) {
             return React.createElement(Label, null, label);
         }
         return null;
     };
-    Input.prototype.renderErrorLabel = function () {
-        var _a = this.props, error = _a.error, errorMessage = _a.errorMessage;
+    Input.prototype.renderErrorLabel = function (error, errorMessage) {
         if (error && errorMessage) {
             return React.createElement(ErrorLabel, null, errorMessage);
         }
         return null;
     };
     Input.prototype.render = function () {
-        var _a = this.props, placeholder = _a.placeholder, disabled = _a.disabled, error = _a.error;
-        return (React.createElement(Wrapper, null,
-            this.renderLabel(),
+        var _a = this.props, label = _a.label, placeholder = _a.placeholder, disabled = _a.disabled, error = _a.error, errorMessage = _a.errorMessage, props = __rest(_a, ["label", "placeholder", "disabled", "error", "errorMessage"]);
+        return (React.createElement(Wrapper, __assign({}, props),
+            this.renderLabel(label),
             React.createElement(StyledInput, { placeholder: placeholder, disabled: disabled, error: error }),
-            this.renderErrorLabel()));
+            this.renderErrorLabel(error, errorMessage)));
+    };
+    Input.defaultProps = {
+        // space
+        m: 0,
     };
     return Input;
 }(React.Component));
