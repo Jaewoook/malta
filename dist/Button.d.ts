1a2f09d515682f2c2b91bb65f74c9c3f7d9e0f79
Sean Soowan Lee <sean@salesbooster.io>
2018-06-08 13:06:29 +0900

    Fix Button.d.ts

--- dist/Button.d.ts
+++ dist/Button.d.ts
@@ -2,6 +2,7 @@
 import * as React from "react";
 export interface IProps {
     onClick?: React.MouseEventHandler<HTMLElement>;
+    type?: string;
     borderColor?: string;
     borderRadius?: number | string;
     border?: string;
