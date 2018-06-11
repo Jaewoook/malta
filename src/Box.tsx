bdc1d60e5da36e1cf3fe6ed08eacba416a1cb189
Jay <pmj933@naver.com>
2018-06-11 22:16:02 +0900

    Add responsiveHide
    Implement responsiveHide function [Box, Img]

--- src/Box.tsx
+++ src/Box.tsx
@@ -1,1 +1,27 @@
-export { Box } from "grid-styled";
+import * as React from "react";
+import styled from "styled-components";
+import { Box as gridStyledBox } from "grid-styled";
+import { breakpoints } from "./theme";
+import { hiddenDown, hiddenUp } from "./tools/responsiveHide";
+
+export interface IProps {
+  hiddendown?: string;
+  hiddenup?: string;
+}
+
+const StyledBox = gridStyledBox.extend`
+  ${(props: IProps) => props.hiddendown && hiddenDown(props.hiddendown)};
+  ${(props: IProps) => props.hiddenup && hiddenUp(props.hiddenup)};
+`;
+
+export class Box extends React.Component<any, any> {
+  public render() {
+    return (
+      <StyledBox
+        {...this.props}
+      >
+        {this.props.children}
+      </StyledBox>
+    );
+  }
+}
