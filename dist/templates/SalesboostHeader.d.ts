4a74142e27700d95db919bb1eb9676640b30baf6
minidonut <nycom13@gmail.com>
2018-08-24 13:16:24 +0900

    Add salesboost header

---
+++ dist/templates/SalesboostHeader.d.ts
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { ResponsiveValue } from "styled-system";
+interface Props {
+    logoSrc: string;
+    headerHeight?: ResponsiveValue<string | number>;
+    imageHeight?: ResponsiveValue<string | number>;
+    bg?: string;
+    padding?: ResponsiveValue<string | number>;
+    onClick?: any;
+    href?: string;
+}
+export declare const SalesboostHeader: React.SFC<Props>;
+export {};
--- src/templates/SalesboostHeader.tsx
+++ src/templates/SalesboostHeader.tsx
@@ -1,14 +1,37 @@
 import * as React from "react";
 import styled from "styled-components";
-import { space, color } from "styled-system";
 import { Flex } from "../atoms/Flex";
+import { Image } from "../atoms/Image";
+import { Box } from "../atoms/Box";
+import { ResponsiveValue } from "styled-system";
 
-type Props = any;
-
-export class SalesboostHeader extends React.Component<Props, any> {
-  public render() {
-    return (
-      <div></div>
-    );
-  }
+interface Props {
+  logoSrc: string;
+  headerHeight?: ResponsiveValue<string | number>;
+  imageHeight?: ResponsiveValue<string | number>;
+  bg?: string;
+  padding?: ResponsiveValue<string | number>;
+  onClick?: any;
+  href?: string;
 }
+
+export const SalesboostHeader: React.SFC<Props> = (props) => {
+  return (
+    <Flex width="100%" height={props.headerHeight} bg={props.bg} alignItems="center" pl={props.padding}>
+      <Box mr="auto" height={props.imageHeight} style={{ cursor: "pointer" }} onClick={props.onClick}>
+        <a href={props.href}>
+          <Image src={props.logoSrc} fit="scale-down" />
+        </a>
+      </Box>
+      {props.children}
+    </Flex>
+  );
+};
+
+SalesboostHeader.defaultProps = {
+  bg: undefined,
+  headerHeight: ["50px", "70px"],
+  imageHeight: ["28px", "34px"],
+  padding: ["8px", "32px"],
+  onClick: () => { }
+};
