63b76ec01442f1c13ba154cc28204ec8266a4fa3
minidonut <nycom13@gmail.com>
2019-01-10 05:10:59 +0900

    [fix] Move location of dist folder

--- dist/core/atom/Image.d.ts
+++
@@ -1,9 +0,0 @@
-import { WidthProps, HeightProps, FlexProps, MaxWidthProps, MaxHeightProps } from "styled-system";
-interface ImgProps {
-    objectFit?: string;
-    objectPosition?: string;
-    alt?: string;
-}
-declare type Props = WidthProps & HeightProps & ImgProps & FlexProps & MaxWidthProps & MaxHeightProps;
-export declare const Image: import("styled-components").StyledComponent<"img", any, Props, never>;
-export {};
