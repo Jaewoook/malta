ae481438dfbc0d3c880b7bca9a19f523450ae0a8
minidonut <nycom13@gmail.com>
2019-01-10 17:13:23 +0900

    [update] shadow값 수정

--- src/salesboost/theme.ts
+++ src/salesboost/theme.ts
@@ -80,11 +80,10 @@ export const radii = ["0", "2px", "4px", "8px", "16px", "32px"];
 
 export const shadows = [
   "none",
-  "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
-  "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
-  "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
-  "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
-  "0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)"
+  "0 2px 4px 0 rgba(22, 27, 72, 0)",
+  "0 4px 8px 0 rgba(22, 27, 72, 0)",
+  "0 8px 16px 0 rgba(22, 27, 72, 0)",
+  "0 16px 32px 0 rgba(22, 27, 72, 0)",
 ];
 
 export const theme = {
