f830c7404abc91c74827f9a90c32a925bf836511
Saehun Chung <saehun.dev@gmail.com>
2019-01-17 23:24:51 +0900

    Feature/portal drawer (#121)
    * [add] Drawer, Portal
    
    * [npm] 0.3.8 -> 0.3.9
    
    * [fix] lint

---
+++ src/salesboost/Portal.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import styled from "styled-components";
+
+export class Portal extends React.Component<any> {
+
+  el: any;
+  modalRoot: any;
+
+  constructor(props: any) {
+    super(props);
+    this.el = document.createElement("div");
+    this.modalRoot = document.getElementById("portal-root");
+  }
+
+  componentDidMount() {
+    this.modalRoot.appendChild(this.el);
+  }
+
+  componentWillUnmount() {
+    this.modalRoot.removeChild(this.el);
+  }
+
+  render() {
+    return ReactDOM.createPortal(
+      this.props.children,
+      this.el,
+    );
+  }
+}
--- src/salesboost/index.ts
+++ src/salesboost/index.ts
@@ -1,5 +1,7 @@
 export * from "./theme";
 export { Select } from "./Select";
 export { Spinner, SpinnerProps } from "./Spinner";
+export { Portal } from "./Portal";
+export { Drawer, DrawerProps } from "./Drawer";
 export { Input } from "./Input";
 export { IconButton, IconButtonProps } from "./IconButton";
