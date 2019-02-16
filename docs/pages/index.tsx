34662a6716af92fdadae3304eb487953027725a6
Jake Jaewook Ahn <ajw4586@gmail.com>
2019-02-17 00:35:45 +0900

    Fix: portal error message of message component

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,10 +1,20 @@
 import * as React from "react";
+import {
+  Button,
+  Message,
+} from "../../packages/malta";
 
 class Index extends React.Component<any, any> {
+
+  state = {
+    show: false,
+  };
 
   render() {
     return (
       <div style={{ paddingTop: "60px" }}>
+        <Message type="info" message="TEST TEST" show={this.state.show} onDismiss={() => this.setState({ show: false })}/>
+        <Button label={"Click"} onClick={() => this.setState({ show: true })}/>
       </div>
     );
   }
