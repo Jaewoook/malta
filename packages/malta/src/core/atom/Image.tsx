72e2a6ae5f88af4694c0b2aa0892a18135500fdd
Jake Jaewook Ahn <ajw4586@gmail.com>
2019-03-18 18:42:00 +0900

    Revert "Fix: Image TypeError with responsive value"
    This reverts commit e7b2d590166cc930dc5e29aab6676a81a4706743.

--- packages/malta/src/core/atom/Image.tsx
+++ packages/malta/src/core/atom/Image.tsx
@@ -1,39 +1,49 @@
 import * as React from "react";
 import styled from "styled-components";
-import { Block } from "./Block";
+import {
+  width,
+  WidthProps,
+  height,
+  HeightProps,
+  flex,
+  FlexProps,
+  maxWidth,
+  MaxWidthProps,
+  maxHeight,
+  MaxHeightProps,
+  space,
+  SpaceProps,
+} from "styled-system";
 
 const objectFit = (props: any) => `object-fit: ${props.objectFit};`;
 const objectPosition = (props: any) => `object-position: ${props.objectPosition};`;
 
-export interface ImageProps {
+interface Props {
   objectFit?: string;
   objectPosition?: string;
   alt?: string;
-  src?: string;
 }
 
-const Img = styled.img<ImageProps>`
+export type ImageProps = WidthProps & HeightProps & Props & FlexProps & MaxWidthProps & MaxHeightProps & SpaceProps;
+
+export const Image = styled.img<ImageProps>`
   box-sizing: border-box;
   flex-shrink: 0;
   overflow: hidden;
-  width: 100%;
-  height: 100%;
+  ${space}
+  ${width}
+  ${height}
   ${objectFit}
   ${objectPosition}
+  ${flex}
+  ${maxWidth}
+  ${maxHeight}
 `;
-
-export const Image = (props: ImageProps) => {
-  const { alt, src, objectFit, objectPosition, ...styles } = props;
-  return <Block {...styles}>
-    <Img src={src} alt={alt} objectFit={objectFit} objectPosition={objectPosition} />
-  </Block>;
-};
 
 Image.defaultProps = {
   width: "100%",
   height: "100%",
   alt: "img",
   objectFit: "cover",
-  objectPosition: "50% 50%",
-  src: "",
+  objectPosition: "50% 50%"
 };
