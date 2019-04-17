3ff48e631604aba3fc6c399c88f6ffc76219edb4
Jake Jaewook Ahn <ajw4586@gmail.com>
2019-04-18 00:21:18 +0900

    Fix: change to export all exported variables except Input

--- packages/malta/src/core/index.ts
+++ packages/malta/src/core/index.ts
@@ -4,7 +4,8 @@ export { Flex, FlexProps } from "./atom/Flex";
 export { Grid, GridProps } from "./atom/Grid";
 export { Icon, IconProps } from "./atom/Icon";
 export { Image, ImageProps } from "./atom/Image";
-export { Input, InputProps } from "./atom/Input";
+// Deprecated component
+// export { Input, InputProps } from "./atom/Input";
 export { Text, TextProps } from "./atom/Text";
 export { Br, BrProps } from "./atom/Br";
 export { Split, SplitProps } from "./atom/Split";
