7ebc0e880f2c23e73feb9f35887cf90ac8525eb0
Jake Jaewook Ahn <ajw4586@gmail.com>
2019-04-24 16:45:15 +0900

    Add: download, wand icon

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
+import {
+  Icon,
+} from "@salesboost/malta";
 
 class Index extends React.Component<any, any> {
 
   render() {
     return (
       <div style={{ paddingTop: "60px" }}>
+        <Icon name="download" color="#f00" />
+        <Icon name="wand" color="#f00" />
       </div>
     );
   }
