6201390a01d503dfc5499bafbe9bc79abe4f42ee
Jake Jaewook Ahn <ajw4586@gmail.com>
2019-07-18 18:13:16 +0900

    Add: default components in playground

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,6 +1,10 @@
 import * as React from "react";
 import {
-  Br,
+  App,
+  Button,
+  Flex,
+  Text,
+  theme,
   Typo,
 } from "@salesboost/malta";
 
@@ -8,26 +12,54 @@ class Index extends React.Component<any, any> {
 
   render() {
     return (
-      <div style={{ padding: "60px" }}>
-        <Typo name="title_1">Title 1</Typo>
-        <Typo name="title_2">Title 2</Typo>
-        <Typo name="title_3">Title 3</Typo>
-        <Typo name="title_4">Title 4</Typo>
-        <Typo name="title_5">Title 5</Typo>
-        <Br />
-        <Typo name="title_3" color="#23c1a6">Custom Title 3</Typo>
-        <Br />
-        <Typo name="body_1">Body 1</Typo>
-        <Typo name="body_2">Body 2</Typo>
-        <Typo name="body_3">Body 3</Typo>
-        <Typo name="body_4">Body 4</Typo>
-        <Br />
-        <Typo name="caption_1">Caption 1</Typo>
-        <Typo name="caption_2">Caption 2</Typo>
-        <Br />
-        <Typo name="highlight_1">Highlight 1</Typo>
-        <Typo name="highlight_2">Highlight 2</Typo>
-      </div>
+      <App theme={theme}>
+        <div style={{ padding: "60px" }}>
+          <Typo name="title_1">Typography</Typo>
+          <Flex mt={4} flexDirection="column">
+            <Flex flexDirection="row" alignItems="baseline">
+              <Typo name="title_1">Title 1</Typo>
+              <Typo ml={2} name="title_2">Title 2</Typo>
+              <Typo ml={2} name="title_3">Title 3</Typo>
+              <Typo ml={2} name="title_4">Title 4</Typo>
+              <Typo ml={2} name="title_5">Title 5</Typo>
+            </Flex>
+            <Flex flexDirection="row" alignItems="baseline">
+              <Typo ml={2} name="body_1">Body 1</Typo>
+              <Typo ml={2} name="body_2">Body 2</Typo>
+              <Typo ml={2} name="body_3">Body 3</Typo>
+              <Typo ml={2} name="body_4">Body 4</Typo>
+            </Flex>
+            <Flex flexDirection="row" alignItems="baseline">
+              <Typo ml={2} name="caption_1">Caption 1</Typo>
+              <Typo ml={2} name="caption_2">Caption 2</Typo>
+              <Typo ml={2} name="highlight_1">Highlight 1</Typo>
+              <Typo ml={2} name="highlight_2">Highlight 2</Typo>
+            </Flex>
+          </Flex>
+          <Typo mt={6} name="title_1">Button</Typo>
+          <Flex mt={4} flexDirection="column">
+            <Typo name="body_1">Normal</Typo>
+            <Flex mt={2} flexDirection="row">
+              <Button label="Solid" />
+              <Button ml={1} line label="Line" />
+            </Flex>
+            <Typo mt={3} name="body_1">Disabled</Typo>
+            <Flex mt={2} flexDirection="row">
+              <Button disabled label="Solid" />
+              <Button ml={1} line disabled label="Line" />
+            </Flex>
+            <Typo mt={3} name="body_1">Loading</Typo>
+            <Flex mt={2} flexDirection="row">
+              <Button loading label="Solid" />
+              <Button ml={1} loading line label="Line" />
+            </Flex>
+          </Flex>
+          <Typo mt={6} name="title_1">Custom</Typo>
+          <Flex flexDirection="column">
+            <Text hover={{ cursor: "pointer", color: "#f00" }} >Hover Text</Text>
+          </Flex>
+        </div>
+      </App>
     );
   }
 }
