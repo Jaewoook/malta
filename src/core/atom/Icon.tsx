13883816288f4518059006a246d5fbec9943c0dd
Jake Jaewook Ahn <ajw4586@gmail.com>
2019-07-23 22:43:30 +0900

    Refactor: Icon and IconButton

--- src/core/atom/Icon.tsx
+++ src/core/atom/Icon.tsx
@@ -1,21 +1,20 @@
 import * as React from "react";
 import styled from "styled-components";
-import { color, ColorProps, ResponsiveValue, space, SpaceProps } from "styled-system";
+import { color, TextColorProps, space, SpaceProps } from "styled-system";
 import { cursor, CursorProps, hover, HoverProps } from "../../utils";
 
 type IconName = "arrow-down" | "arrow-left" | "arrow-right" | "arrow-up" | "back" | "check" | "circle-info" | "circle-question" | "circle-warning" | "circle-success" | "circle-error" | "close" | "crown" | "setting" | "giventake" | "gridview" | "helpbadge" | "listview" | "next" | "search" | "selectall" | "step" | "upload" | "reload" | "add" | "delete" | "doublearrow-left" | "doublearrow-right" | "edit" | "function" | "home" | "ascending" | "descending" | "logout" | "personal" | "support" | "link" | "global" | "guide" | "folder" | "hamburger" | "wand" | "download" | "statistics";
 
-interface Props {
+export interface IconProps extends HoverProps, TextColorProps, CursorProps, SpaceProps {
   name: IconName;
-  theme?: any;
-  size?: ResponsiveValue<number>;
+  size?: number;
 }
 
-const StyledSvg = styled.svg.attrs((props: any) => ({
+const StyledSvg = styled.svg.attrs((props: { size?: number }) => ({
   xmlns: "http://www.w3.org/2000/svg",
   width: props.size,
   height: props.size,
-})) <any>`
+}))<any>`
   box-sizing: border-box;
   ${color}
   ${hover}
@@ -24,9 +23,7 @@ const StyledSvg = styled.svg.attrs((props: any) => ({
   transition: all 0.15s ease-out;
 `;
 
-export type IconProps = Props & HoverProps & ColorProps & CursorProps & SpaceProps;
-
-export const Icon: React.SFC<IconProps> = (props) => {
+export const Icon: React.FC<IconProps> = (props) => {
   const ic = getIcons(props.name);
   return (
     <StyledSvg {...props} viewBox={`0 0 ${ic.width} ${ic.height}`} >
